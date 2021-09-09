122e89678e273662bf8f36868bff86f820cf4365
ABHINAV PATHAK <87684426+GhostXBeast@users.noreply.github.com>
2021-09-09 20:59:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 API_ID = int(os.getenv("API_ID", "6085437"))
 API_HASH = os.getenv("API_HASH", "6db33aae3e34ab6c2a9623296b3403c0")
-BOT_TOKEN = os.getenv("BOT_TOKEN","1931356903:AAHQ-Ep7XvY9NbkQGf0P-ApPyDu8aMpRAnU"")
+BOT_TOKEN = os.getenv("BOT_TOKEN","1931356903:AAHQ-Ep7XvY9NbkQGf0P-ApPyDu8aMpRAnU")
 SESSION_NAME = os.getenv("SESSION_NAME","BQBIu4XQuy6qW5HTvp3oTTk3-rw6Vq3MzlWetei_hi5OM04EnUVnPgC_FOapZVBruxVHyZRVVSA5R52e6RDOyqaRUwPLcQbfiN6P93rJ6rdZakywALNsuL4a8YjS1NSq0nxqI0IbXKRGM8Ho9U7ZAua3pozDi4cvB292sudemV-Q9m95mfzwSqiKkAtR4SEnXdOhsDUxyWJxPRBX3GqLO03SMBolhZBDlYUHcdN5_NXC0p7kkotXYHXzzcB57HJ1ZEcuO70fVbgT0szMJ8yD2Z7Kvt5s7lHFOEBjIJomT6VfQuIYAfpp0yLv13NxM6fpRE_YvDb1gBpjEifYlLdK0bTPc5VVAwA")
 ADMIN = os.getenv("ADMIN","1939166467")
 DB_URL = os.getenv("DB_URL","mongodb+srv://Anon956:GlACEljYJAEEOI9k@cluster0.bng7u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
